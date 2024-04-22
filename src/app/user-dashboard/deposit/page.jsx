702a5229abe00ac4876f8f787a80feaa63a3c0fb
Tijesuni333 <99971409+Tijesuni333@users.noreply.github.com>
2024-04-22 19:49:10 +0100

    new

--- src/app/user-dashboard/deposit/page.jsx
+++ src/app/user-dashboard/deposit/page.jsx
@@ -1,22 +1,21 @@
 export default function Deposit(){
     return (
-        <div className="deposit">
+        <div className="p-24 m-80 mt-40 mb-0 bg-gray-100" id="deposit">
             <form>
-                <h3>Deposit</h3>
+                <h3 className="font-bold text-xl">Deposit</h3>
 
-                <label>Token:</label>
-                <select>
+                <label className="block p-4 px-0 pb-1">Token:</label>
+                <select className="inline-block w-full h-12">
                     <option value="1">BTC  </option>
                     <option value="2">ETH </option>
                     <option value="3">BNB </option>
                     <option value="4">MET</option>
                 </select>
                 <p>Total balance: 1234455</p>
-                <label >Address:</label>
-                <input type="text" />
+                <label className="block p-4 px-0 pb-1" >Address:</label>
+                <input type="text"  className="inline-block w-full h-12"/>
 
-
-                <button>Deposit</button>
+                <button className="block m-8 p-8 pt-4 pb-4 bg-gray-200">Deposit</button>
             </form>
         </div>
     )
