0eff280e991435dcdc0e72438674826b7ff756c5
Viktohblake <damanygt@gmail.com>
2024-05-30 20:16:51 +0100

    update dao vote and proposal info ui

--- src/components/dao/vote-result.jsx
+++ src/components/dao/vote-result.jsx
@@ -1,48 +1,35 @@
 import React from "react";
 
-export default function VoteResult({ yesVotes,
-    noVotes }) {
-
-        // Calculate the total number of votes
-        const totalVotes = yesVotes + noVotes;
-
-        // Calculate the percentage of votes for each option
-        const yesPercentage = (yesVotes / totalVotes) * 100;
-        const noPercentage = (noVotes / totalVotes) * 100;
-
-  return (
-    <div className="border border-slate-800 rounded-lg p-4 mt-4 max-w-sm">
-      <h2 className="text-lg font-semibold mb-2">Vote Result</h2>
-      <div className="flex flex-col gap-2">
-        <div className="items-center">
-            <div className="flex justify-between">
-          <p className="font-bold text-sm mr-2">Yes:</p>
-          <p className="font-bold text-sm ml-2">{yesPercentage.toFixed(1)}%</p>
-            </div>
-
-          <div className="w-full h-2 bg-gray-300 rounded-lg overflow-hidden">
-            <div
-              className="h-full bg-white"
-              style={{ width: `${yesPercentage}%` }}
-            ></div>
-          </div>
+export default function VoteResult({ totalVotes, requiredVotes, proposalStatus }) {
+   // Check if the proposal is ongoing
+   if (proposalStatus === true) {
+    return (
+        <div className="border border-slate-800 rounded-lg p-4 mt-4 max-w-sm">
+            <h2 className="text-lg font-semibold mb-2">Vote Result</h2>
+            <p className="font-bold text-sm">Proposal Ongoing</p>
         </div>
-        <div className="items-center">
-            <div className="flex justify-between">
-          <p className="font-bold text-sm mr-2">No:</p>
-          <p className="font-bold text-sm ml-2">{noPercentage.toFixed(1)}%</p>
+    );
+} else {
+  // Calculate the percentage of total votes
+    const totalPercentage = (Number(totalVotes) / Number(requiredVotes)) * 100;
 
+    // Check if the proposal passed
+    const proposalPassed = totalPercentage >= 66.67; // 2/3 in percentage
+
+    return (
+        <div className="border border-slate-800 rounded-lg p-4 mt-4 max-w-sm">
+            <h2 className="text-lg font-semibold mb-2">Vote Result</h2>
+            <div className="flex flex-col gap-2">
+                {proposalPassed ? (
+                    <p className="text-green-500 font-semibold">Proposal passed!</p>
+                ) : (
+                    <p className="text-red-500 font-semibold">Proposal failed to pass.</p>
+                )}
             </div>
-          <div className="w-full h-2 bg-gray-300 rounded-lg overflow-hidden">
-            <div
-              className="h-full bg-white"
-              style={{ width: `${noPercentage}%` }}
-            ></div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
+}
+    
 export { VoteResult };
