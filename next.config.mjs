7d9c8028ffd9497a4d93201f6a54b83531988c28
JeffreyJoel <jowoloko@gmail.com>
2024-06-02 11:47:52 +0100

    add pino-pretty

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   images: {
     domains: ["cdn.stamp.fyi", "aquamarine-famous-penguin-727.mypinata.cloud"],
   },
-  
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  }
 };
 
 export default nextConfig;
